d6114b5afa86ceb2427294a06d144d976f870063
jhm <maxosen64@gmail.com>
2024-08-29 22:35:24 +0800

    f

--- frontend-store/vitest.config.ts
+++ frontend-store/vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   test: {
     // ...
     environment: "jsdom",
+    reporters: process.env.GITHUB_ACTIONS ? ["github-actions"] : ["default"],
   },
   resolve: {
     alias: {
