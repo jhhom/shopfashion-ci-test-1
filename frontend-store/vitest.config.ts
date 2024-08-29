cd1f6292be32476202766906130e9b4ff84749b0
jhm <maxosen64@gmail.com>
2024-08-30 00:06:07 +0800

    fa

--- frontend-store/vitest.config.ts
+++ frontend-store/vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     // ...
-    // environment: "jsdom",
+    environment: "jsdom",
     reporters: ["github-actions"],
   },
   resolve: {
